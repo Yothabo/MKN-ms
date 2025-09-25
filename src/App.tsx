ec25a1074dd9a99de8d829100938c0b0eb43b706
Miniyothabo <miniyothab@gmail.com>
2025-09-25 16:12:24 +0200

    Add NotFound.tsx and update App.tsx with catch-all route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-
-// Import the Entry page from its location
 import Entry from './pages/entry/index.tsx';
-
-// Import the main admin page from the root of its directory
 import AdminLayout from './pages/admin/index.tsx';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Entry />} />
       <Route path="/admin/*" element={<AdminLayout />} />
+      <Route path="*" element={<NotFound />} /> {/* Catch-all */}
     </Routes>
   );
 }
 
 export default App;
-
